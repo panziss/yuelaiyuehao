ac4eee5862d3d422d7aa0e37a98db7997657c088
nathan <1121380402@qq.com>
2018-03-20 16:45:49 +0800

    aaaa

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 $(".navFu_nav_ul2 li").mouseover(function(){
 	$(this).addClass("active").siblings().removeClass("active");
 })
@@ -10,23 +8,23 @@ $(".navFu_nav_ul2 li").click(function(){
 })
 /**********content1 hover效果******************/
 $(".content1 ul li").hover(function(){
-	$(this).find("a").css({"background":"url(./image/cnt1_bg1.png)"});
-	$(this).find("dt").css({"background":"url(./image/cnt1_ico2.png) no-repeat center bottom","color":"#fff"});
+	$(this).find("a").css({"background":"url(./images/cnt1_bg1.png)"});
+	$(this).find("dt").css({"background":"url(./images/cnt1_ico2.png) no-repeat center bottom","color":"#fff"});
 	$(this).find("dd").css("color","#fff");
 },function(){
 	$(this).find("a").css({"background":"none"});
-	$(this).find("dt").css({"background":"url(./image/cnt1_ico1.png) no-repeat center bottom","color":"#f3658e"});
+	$(this).find("dt").css({"background":"url(./images/cnt1_ico1.png) no-repeat center bottom","color":"#f3658e"});
 	$(this).find("dd").css("color","#999");
 })
 
 /*****************实现培训课程的内容切换**************/
 $(function(){
 	$(".content2 .list li").click(function(){
-		$(".content2 .list li").find("img").attr('src',"image/cnt2_ico3.png" );
+		$(".content2 .list li").find("img").attr('src',"images/cnt2_ico3.png" );
 		$(".content2 .list li").find("img").css("top","0px")
 		$(".content2 .list li").find("p").removeClass("active")
 		$(this).find("p").addClass("active")
-		$(this).find("img").attr('src',"image/cnt2_ico4.png" ).css("top","-10px");
+		$(this).find("img").attr('src',"images/cnt2_ico4.png" ).css("top","-10px");
 	})
 	$(".content2 .lt_item1 li").click(function(){
 		$(".content2 .rt li").removeClass("active");
@@ -89,14 +87,6 @@ $(".content3_cont_an .an2").click(function(){
 });
 
 $(".content3_ul_p img").click(function(){
-	
-//	$(".content3_ul li").eq(0).css("left","0px")
-//	$(".content3_ul li").eq(1).css("left","240px")
-//	$(".content3_ul li").eq(2).css("left","480px")
-//	$(".content3_ul li").eq(3).css("left","720px")
-//	$(".content3_ul li").eq(4).css("left","960px")
-//	$(".content3_ul li").eq(5).css("left","1200px")
-//	$(".content3_ul li").eq(6).css("left","1440px")
 	var le=parseInt($(".content3_ul .active").css("left"));
 	     $(".content3_ul .active").css("left",le+10);
 	 $(this).parent().parent().addClass("active").siblings().removeClass("active");
@@ -104,3 +94,19 @@ $(".content3_ul_p img").click(function(){
 		$(this).parents("li").css("left",left-10);
 	
 })
+
+$(function(){
+	$(".page3_content_ul li").hover(function(){
+		$(".page3_content_p").css({'display':'none'});
+		$(".page3_content_p").eq($(this).index()).css({'display':'block'});
+	},function(){
+		$(".page3_content_p").css({'display':'none'});
+	});
+	
+	var length = $(".page4_content_ul li").length;
+	for(var i= 1;i<=length;i++){
+		$(".page4_content_ul li").eq(i-1).css({'background':'url(images/page4_a'+i+'.jpg) no-repeat'});
+	}
+})
+
+
